a95403e1a0bfca5550bb83f81d017d9bcad1e3d6
AgneKal <157694253+AgneKal@users.noreply.github.com>
2024-10-06 20:34:03 +0300

    test 4 and 5 done

--- cypress/e2e/test04.cy.js
+++ cypress/e2e/test04.cy.js
@@ -6,10 +6,30 @@ describe('Verify All Products and product detail page', () => {
         cy.launchBrowserNavigateHome();
 
         // 4. Click on 'Products' button
+        cy.get('.nav > li > a').contains('Products').click();
+
         // 5. Verify user is navigated to ALL PRODUCTS page successfully
+        cy.get('.features_items > h2').contains('All Products').should('be.visible');
+        cy.url().should('include', '/products');
+
         // 6. The products list is visible
+        cy.get('.features_items').should('be.visible');
+        cy.get('.features_items').find('.col-sm-4').should('have.length.greaterThan', 0);
+
         // 7. Click on 'View Product' of first product
+        cy.get('.choose > ul.nav > li > a').first().click();
+
         // 8. User is landed to product detail page
+        cy.url().should('include', '/product_details/1');
+
         // 9. Verify that detail detail is visible: product name, category, price, availability, condition, brand
+        cy.get('.product-information').within(() => {
+            cy.get('h2').should('be.visible');
+            cy.get('p').contains('Category').should('be.visible');
+            cy.get('span').contains('Rs').should('be.visible');
+            cy.get('p').contains('Availability').should('be.visible');
+            cy.get('p').contains('Condition').should('be.visible');
+            cy.get('p').contains('Brand').should('be.visible');
+        });
     });
 });
--- cypress/e2e/test05.cy.js
+++ cypress/e2e/test05.cy.js
@@ -6,8 +6,16 @@ describe('Verify Subscription in home page', () => {
         cy.launchBrowserNavigateHome();
 
         // 4. Scroll down to footer
+        cy.scrollTo('bottom');
+
         // 5. Verify text 'SUBSCRIPTION'
+        cy.get('footer h2').should('have.text', 'Subscription');
+
         // 6. Enter email address in input and click arrow button
+        cy.get('footer input[type="email"]').type('dumbemail@email.com');
+        cy.get('button[type="submit"]').click();
+
         // 7. Verify success message 'You have been successfully subscribed!' is visible
+        cy.get('footer #success-subscribe').should('be.visible');
     });
 });
